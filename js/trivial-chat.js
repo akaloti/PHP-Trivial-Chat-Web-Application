b90a081751e411411db8a5ca6914b4f391dbf83a
aaronistheman <aarons.7007@gmail.com>
2015-12-08 16:19:11 -0800

    A user can "log in"
    Currently, attempting to log in does nothing more than report to the user
    if a valid pair of a username and a password was given.

--- js/trivial-chat.js
+++ js/trivial-chat.js
@@ -21,15 +21,41 @@ $(document).ready(function() {
                 },
                 function(json, status) {
                     if (json.success) {
-                        alert("Success!");
+                        alert("New account created!");
                     }
                     else {
                         alert("Name already taken!");
+                        $("#create-pw").val("");
                     }
                 }
-            )
+            );
         }
 
         e.preventDefault();
+    });
+
+    $("#login-login").click(function(e) {
+        $.getJSON(
+            "login.php",
+            {
+                name: $("#login-name").val(),
+                pw: $("#login-pw").val()
+            },
+            function(json, status) {
+                if (json.success) {
+                    alert("Successful login!");
+                }
+                else if (json.scriptError) {
+                    // Report the error from the PHP script
+                    var message = json.scriptErrorMessage;
+                    alert(message);
+                    console.log(message);
+                }
+                else {
+                    alert("Wrong username or password");
+                    $("#login-pw").val("");
+                }
+            }
+        );
     });
 });
