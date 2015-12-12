53f9ef06ad08b757c7dc4517b660f24621601a09
aaronistheman <aarons.7007@gmail.com>
2015-12-11 16:57:40 -0800

    Created a table for storing chat room messages

--- setup.php
+++ setup.php
@@ -1,11 +1,35 @@
 <?php
-    // File for creating the table; should only have to be called once ever
+    /**
+     * File for creating the table;
+     * should only have to be called once ever
+     */
 
     include 'dbconnect.php';
 
+    $usernameSize = 40;
+
     try {
-        $queryStr = "CREATE TABLE users (name VARCHAR(40),
+        /**
+         * Table: users
+         *
+         * Columns:
+         * @name the user's name
+         * @password the user's password
+         * @lastupdate the last time that the user received others' messages
+         */
+        $queryStr = "CREATE TABLE users (name VARCHAR(".$usernameSize."),
             password VARCHAR(100), lastupdate timestamp DEFAULT NULL)";
+        $db->query($queryStr);
+
+        /**
+         * Table: messages
+         *
+         * Columns:
+         * @name the user who sent the message
+         * @message the content of the message
+         */
+        $queryStr = "CREATE TABLE messages (name VARCHAR(".$usernameSize."),
+            message VARCHAR(1000))";
         $db->query($queryStr);
     }
     catch (PDOException $e) {
